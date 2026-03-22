f403594bd624267991fe575f7797c28eb234de66
0ayoub01 <ayoubbaroudi43@gmail.com>
2026-03-22 14:01:43 +0100

    fix: add missing data files locations.js and delegationCoords.js

---
+++ frontend/src/data/delegationCoords.js
@@ -0,0 +1,63 @@
+// Approximate coordinates (Lat, Lng) for major Tunisian delegations
+// Used as a fallback when reverse geocoding cannot name a specific area.
+export const delegationCoords = [
+  // Ariana
+  { name: "Ariana Ville", gov: "Ariana", lat: 36.8625, lng: 10.1956 },
+  { name: "La Soukra", gov: "Ariana", lat: 36.8833, lng: 10.2500 },
+  { name: "Raoued", gov: "Ariana", lat: 36.9167, lng: 10.1833 },
+  { name: "Kalaat el-Andalous", gov: "Ariana", lat: 37.0633, lng: 10.1186 },
+  { name: "Sidi Thabet", gov: "Ariana", lat: 36.9083, lng: 10.0433 },
+  { name: "Ettadhamen", gov: "Ariana", lat: 36.8375, lng: 10.1233 },
+  { name: "Mnihla", gov: "Ariana", lat: 36.8417, lng: 10.1111 },
+
+  // Tunis
+  { name: "Tunis City", gov: "Tunis", lat: 36.8065, lng: 10.1815 },
+  { name: "Carthage", gov: "Tunis", lat: 36.8528, lng: 10.3236 },
+  { name: "La Marsa", gov: "Tunis", lat: 36.8781, lng: 10.3247 },
+  { name: "Le Bardo", gov: "Tunis", lat: 36.8094, lng: 10.1406 },
+  { name: "Le Kram", gov: "Tunis", lat: 36.8308, lng: 10.3164 },
+  { name: "La Goulette", gov: "Tunis", lat: 36.8181, lng: 10.3050 },
+  { name: "Sidi Hassine", gov: "Tunis", lat: 36.7667, lng: 10.1167 },
+  { name: "El Menzah", gov: "Tunis", lat: 36.8433, lng: 10.1708 },
+
+  // Sousse
+  { name: "Sousse Ville", gov: "Sousse", lat: 35.8256, lng: 10.6369 },
+  { name: "Hammam Sousse", gov: "Sousse", lat: 35.8600, lng: 10.5900 },
+  { name: "Akouda", gov: "Sousse", lat: 35.8700, lng: 10.5600 },
+  { name: "Kalâa Kebira", gov: "Sousse", lat: 35.8667, lng: 10.5333 },
+  { name: "Msaken", gov: "Sousse", lat: 35.7333, lng: 10.5833 },
+  { name: "Enfidha", gov: "Sousse", lat: 36.1333, lng: 10.3833 },
+
+  // Sfax
+  { name: "Sfax Ville", gov: "Sfax", lat: 34.7400, lng: 10.7600 },
+  { name: "Sakiet Ezzit", gov: "Sfax", lat: 34.8000, lng: 10.7667 },
+  { name: "Sakiet ed-Daïer", gov: "Sfax", lat: 34.7833, lng: 10.8167 },
+  
+  // Bizerte
+  { name: "Bizerte Nord", gov: "Bizerte", lat: 37.2744, lng: 9.8739 },
+  { name: "Menzel Bourguiba", gov: "Bizerte", lat: 37.1500, lng: 9.7833 },
+  
+  // Nabeul
+  { name: "Nabeul", gov: "Nabeul", lat: 36.4561, lng: 10.7335 },
+  { name: "Hammamet", gov: "Nabeul", lat: 36.4000, lng: 10.6167 },
+  
+  // Monastir
+  { name: "Monastir", gov: "Monastir", lat: 35.7833, lng: 10.8333 },
+  
+  // Gabès
+  { name: "Gabès Ville", gov: "Gabès", lat: 33.8814, lng: 10.0983 },
+  
+  // Medenine
+  { name: "Houmt El Souk", gov: "Médenine", lat: 33.8750, lng: 10.8575 },
+  { name: "Midoun", gov: "Médenine", lat: 33.8000, lng: 11.0000 },
+  { name: "Zarzis", gov: "Médenine", lat: 33.5000, lng: 11.1167 },
+
+  // Jendouba
+  { name: "Tabarka", gov: "Jendouba", lat: 36.9500, lng: 8.7500 },
+
+  // Gafsa
+  { name: "Gafsa Ville", gov: "Gafsa", lat: 34.4250, lng: 8.7842 },
+
+  // Kairouan
+  { name: "Kairouan Ville", gov: "Kairouan", lat: 35.6781, lng: 10.0964 }
+];
---
+++ frontend/src/data/locations.js
@@ -0,0 +1,74 @@
+export const tunisianLocations = {
+  "Tunis": [
+    "Tunis City", "Carthage", "La Marsa", "Le Bardo", "Le Kram", "La Goulette", "Sidi Hassine", "Sidi El Bechir", "El Omrane", "El Omrane Superieur", "Etahrir", "El Menzah", "Bab El Bhar", "Bab Souika", "El Ouardia", "El Kabaria", "Jebel Jelloud", "Ezzouhour", "Hrairia"
+  ],
+  "Ariana": [
+    "Ariana Ville", "La Soukra", "Raoued", "Kalaat el-Andalous", "Sidi Thabet", "Ettadhamen", "Mnihla"
+  ],
+  "Ben Arous": [
+    "Ben Arous", "Bou Mhel el-Basstine", "El Mourouj", "Ezzahra", "Fouchana", "Hammam Chott", "Hammam Lif", "Mohamedia", "Medina Jedida", "Mornag", "Radès"
+  ],
+  "Manouba": [
+    "Manouba", "Den Den", "Douar Hicher", "Oued Ellil", "Mornaguia", "Borj El Amri", "Tebourba", "El Batan"
+  ],
+  "Nabeul": [
+    "Nabeul", "Dar Chaâbane el-Fehri", "Béni Khiar", "Korba", "Menzel Temime", "Kélibia", "Hammam Ghezèze", "El Haouaria", "Takelsa", "Soliman", "Menzel Bouzelfa", "Béni Khalled", "Grombalia", "Bou Argoub", "Hammamet"
+  ],
+  "Zaghouan": [
+    "Zaghouan", "Zriba", "Bir Mcherga", "Nadhour", "El Fahs", "Saouaf"
+  ],
+  "Bizerte": [
+    "Bizerte Nord", "Bizerte Sud", "Sejnane", "Joumine", "Mateur", "Ghazala", "Utique", "Ghar El Melh", "Ras Jebel", "Alia", "Menzel Bourguiba", "Menzel Jemil"
+  ],
+  "Béja": [
+    "Béja Nord", "Béja Sud", "Amdoun", "Nefza", "Téboursouk", "Tibar", "Testour", "Goubellat", "Majaz al Bab"
+  ],
+  "Jendouba": [
+    "Jendouba", "Jendouba Nord", "Bou Salem", "Tabarka", "Aïn Draham", "Fernana", "Ghardimaou", "Oued Mliz", "Balta-Bou Aouane"
+  ],
+  "Le Kef": [
+    "Le Kef Est", "Le Kef Ouest", "Nebeur", "Sakiet Sidi Youssef", "Tajerouine", "Kalaat Senan", "Kalâat Khasba", "Jérissa", "El Ksour", "Dahmani", "Sers"
+  ],
+  "Siliana": [
+    "Siliana Nord", "Siliana Sud", "Bou Arada", "Gaâfour", "El Krib", "Bourouis", "Makthar", "Rouhia", "Kesra", "Bargou"
+  ],
+  "Kairouan": [
+    "Kairouan Nord", "Kairouan Sud", "Chebika", "Sbikha", "Oueslatia", "Aïn Djeloula", "Haffouz", "Alâa", "Hajeb El Ayoun", "Nasrallah", "Bou Hajla", "Cherarda"
+  ],
+  "Kasserine": [
+    "Kasserine Nord", "Kasserine Sud", "Sbeïtla", "Fériana", "Thala", "Haidra", "Jedelienne", "El Ayoun", "Majel Bel Abbès", "Sbiba", "Ezzouhour", "Foussana"
+  ],
+  "Sidi Bouzid": [
+    "Sidi Bouzid Ouest", "Sidi Bouzid Est", "Jilma", "Cebbalat Ouled Asker", "Bir El Hafey", "Sidi Ali Ben Aoun", "Menzel Bouzaiane", "Meknassy", "Souk Jedid", "Regueb", "Ouled Haffouz", "Mezzouna"
+  ],
+  "Sousse": [
+    "Sousse Ville", "Sousse Jawhara", "Sousse Riadh", "Sousse Sidi Abdelhamid", "Hammam Sousse", "Akouda", "Kalâa Kebira", "Kalâa Seghira", "Sidi Bou Ali", "Hergla", "Enfidha", "Bouficha", "Kondar", "Sidi El Hani", "Msaken"
+  ],
+  "Monastir": [
+    "Monastir", "Ouardanine", "Sahline", "Zéramdine", "Béni Hassen", "Ghemmada", "Jammel", "Bembla", "Moknine", "Bakalta", "Sayada-Lamta-Bou Hajar", "Ksar Hellal", "Ksibet el-Médiouni"
+  ],
+  "Mahdia": [
+    "Mahdia", "Bou Merdes", "Ouled Chamekh", "Chorbane", "Hebira", "Ksour Essef", "Chebba", "Melloulèche", "Sidi Alouane", "El Jem", "Souassi"
+  ],
+  "Sfax": [
+    "Sfax Ville", "Sfax Ouest", "Sfax Sud", "Sakiet Ezzit", "Sakiet ed-Daïer", "Thyna", "Agareb", "Jebiniana", "El Amra", "El Hencha", "Menzel Chaker", "Graïba", "Bir Ali Ben Khalifa", "Skhira", "Maharès", "Kerkennah"
+  ],
+  "Gafsa": [
+    "Gafsa Nord", "Gafsa Sud", "Moularès", "Redeyef", "Métlaoui", "Mdhilla", "El Ksar", "Belkhir", "Sened", "Sidi Aïch", "Guétoucha"
+  ],
+  "Tozeur": [
+    "Tozeur", "Degache", "Tamaghza", "Nefta", "Hazoua"
+  ],
+  "Kebili": [
+    "Kebili Nord", "Kebili Sud", "Douz Nord", "Douz Sud", "Souk Lahad", "Faouar"
+  ],
+  "Gabès": [
+    "Gabès Ville", "Gabès Ouest", "Gabès Sud", "Ghannouch", "Métouia", "Mareth", "Matmata", "Nouvelle Matmata", "Menzel El Habib", "El Hamma"
+  ],
+  "Médenine": [
+    "Médenine Nord", "Médenine Sud", "Béni Khedache", "Ben Guerdane", "Zarzis", "Houmt El Souk", "Midoun", "Ajim", "Sidi Makhlouf"
+  ],
+  "Tataouine": [
+    "Tataouine Nord", "Tataouine Sud", "Bir Lahmar", "Ghomrassen", "Dehiba", "Remada", "Smâr"
+  ]
+};
